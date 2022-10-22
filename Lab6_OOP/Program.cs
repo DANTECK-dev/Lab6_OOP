afc7752b14c4609edf0efed336e9496e637bbf14
Александр Немеров <nemerov_a@mail.ru>
2022-10-23 00:21:25 +0800

    Done

--- Lab6_OOP/Program.cs
+++ Lab6_OOP/Program.cs
@@ -1,42 +1,111 @@
 ﻿using System;
+using MyList;
 
 namespace Lab6_OOP
 {
     public class Program
     {
-        static int?[] arr = new int?[100];
+        static readonly int?[] arr = new int?[100];
         static void Main(string[] _)
         {
             Task1();
             Task2();
+            Indiv();
         }
         static void Task1()
         {
-            Random rand = new Random();
-            int? k = null;
             Console.Write("число N ");
             int n = Convert.ToInt32(Console.ReadLine());
             for(int i = 0; i < n; i++)
             {
                 arr[i] = null;
-                arr[rand.Next(arr.Length)] = i;
+                arr[new Random().Next(arr.Length)] = i;
             }
-            foreach(var x in arr)
-            {
-                if(x == null) Console.Write("null ");
-                Console.Write(x + " ");
-            }
+            Print(arr);
         }
         static void Task2()
         {
-            int? p = arr[arr.Length / 2];
-            int i = 0;
-            int j = arr.Length - 1;
-            
+            QuickSort(arr, 0, arr.Length - 1);
+            Array.Reverse(arr);
+            Print(arr);
+            Console.WriteLine();
         }
-        private void quickSort(int?[] massive, int begin, int end)
+        static private void QuickSort(int?[] arr, int begin, int end)
         {
+            if (end < 0)
+                end = arr.Length - 1;
+            if (begin >= end)
+                return;
+            int middleIndex = (end - begin) / 2 + begin, currentIndex = begin;
+            Swap(arr, begin, middleIndex);
+            for (int i = begin + 1; i <= end; ++i)
+            {
+                if (arr[i] <= arr[begin] || arr[i] == null)
+                {
+                    currentIndex++;
+                    Swap(arr, currentIndex, i);
+                }
+            }
+            Swap(arr, begin, currentIndex);
+            QuickSort(arr, begin, currentIndex - 1);
+            QuickSort(arr, currentIndex + 1, end);
+        }
+        static private void Swap(int?[] arr, int i, int j)
+        {
+            (arr[j], arr[i]) = (arr[i], arr[j]);
+        }
+        static private void Print(int?[] arr)
+        {
+            for(int i = 0; i < arr.Length; ++i)
+            {
+                if (i % 10 == 0) Console.WriteLine();
+                if (arr[i] == null) Console.Write("null ");
+                else Console.Write(String.Format("{0,-5:D}",arr[i]));
+            }
+            Console.WriteLine();
+        }
+        static private void Indiv()
+        {
+            ListQueue listQueue = new();
+            listQueue.Add(new Person("Андреев", "Алексей", "Даниилович"));
+            listQueue.Add(new Person("Степанов", "Иван", "Павлович"));
+            listQueue.Add(new Person("Платонова", "Марьям", "Максимовна"));
+            listQueue.Add(new Person("Федоров", "Всеволод", "Кириллович"));
+            listQueue.Add(new Person("Муравьев", "Роман", "Сергеевич"));
+            listQueue.Add(new Person("Макаров", "Мирослав", "Артемьевич"));
+            listQueue.Add(new Person("Фомина", "Серафима", "Германовна"));
+            listQueue.Add(new Person("Казаков", "Иван", "Кириллович"));
+            listQueue.Add(new Person("Цветков", "Билал", "Михайлович"));
+            listQueue.Add(new Person("Попова", "Александра", "Фёдоровна"));
+            listQueue.Add(new Person("Вдовина", "Екатерина", "Семёновна"));
+            listQueue.Add(new Person("Петрова", "Диана", "Ивановна"));
+            listQueue.Add(new Person("Федоров", "Всеволод", "Кириллович"));
+            listQueue.Add(new Person("Копылова", "Ева", "Юрьевна"));
 
+            listQueue.GetMassive();
+
+            listQueue.Null(3);
+            listQueue.Null(5);
+            listQueue.Null(6);
+            listQueue.Null(2);
+            listQueue.Null(7);
+            listQueue.Null(20);
+            listQueue.Get(20);
+
+            listQueue.Print();
+
+            listQueue.Add(new Person("dfg", "zxcv", "dsf"));
+            listQueue.Add(new Person("dfg", "zxcv", "dsf"));
+            listQueue.Add(new Person("dfg", "zxcv", "dsf"));
+            listQueue.Add(new Person("dfg", "zxcv", "dsf"));
+            listQueue.Add(new Person("dfg", "zxcv", "dsf"));
+            listQueue.Add(new Person("dfg", "zxcv", "dsf"));
+            listQueue.Add(new Person("dfg", "zxcv", "dsf"));
+
+            listQueue.Print();
+
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
---
+++ MyList/MyList.cs
@@ -0,0 +1,223 @@
+﻿namespace MyList
+{
+    interface IListQueue
+    {
+        public void Add(Person person);
+        public Person? Remove();
+        public void GetMassive();
+    }
+    public class Person
+    {
+        private string? Name { get; }
+        private string? Surname { get; }
+        private string? Patronymic { get; }
+
+        public Person(string Name, string Surname, string Patronymic)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Patronymic = Patronymic;
+        }
+        public override string ToString()
+        {
+            return Name + " " + Surname + " " + Patronymic;
+        }
+    }
+    public class Node
+    {
+        public Person? Data { get; set; }
+        public Node? Next { get; set; }
+        public Node? Prev { get; set; }
+        public override string? ToString()
+        {
+            return Data?.ToString();
+        }
+    }
+    public class ListQueue : IListQueue
+    {
+        private Node? head = null;
+        private Node? tail = null;
+        int count = 0;
+        public int Count
+        {
+            get { return count; }
+            private set
+            {
+                if (count < 0) throw new ArgumentNullException("count", "Счётчик не может быть меньше нуля");
+                count = value;
+            }
+        }
+        public void Add(Person person)
+        {
+            for(int i = 0; i < Count; i++)
+            {
+                if (head is null || tail is null) break;
+                if (Get(i) is null)
+                {
+                    Set(person, i);
+                    Console.WriteLine(person.ToString() + " Added");
+                    return;
+                }
+            }
+            Node? node = new()
+            {
+                Data = person
+            };
+            if (head is null)
+                head = node;
+            else if (head.Next is null)
+                head.Next = node;
+
+            if (tail is not null)
+                tail.Next = node;
+            node.Prev = tail;
+            tail = node;
+
+            count++;
+            Console.WriteLine(person.ToString() + " Added");
+        }
+        public Person? Remove()
+        {
+            if (head is null || tail is null)
+            {
+                Console.WriteLine("ListQueue is empty");
+                tail = null;
+                return null;
+            }
+            var person = head.Data;
+            if (person == null)
+            {
+                Console.WriteLine("ListQueue is empty");
+                return null;
+            }
+            head = head.Next;
+            head!.Prev = null;
+
+            Console.WriteLine(person.ToString() + " Removed");
+            count--;
+            return person;
+        }
+        public Person? Get(int index)
+        {
+            if (head is null || tail is null)
+            {
+                Console.WriteLine("ListQueue is empty");
+                return null;
+            }
+            if (index >= count)
+            {
+                Console.WriteLine("Out in the range");
+                return null;
+            }
+
+            Node? cur = head;
+            for (int i = 0; i < count; i++)
+            {
+                if (index == i)
+                {
+                    return cur?.Data;
+                }
+                cur = cur?.Next;
+            }
+            return null;
+        }
+        public void Set(Person? person, int index)
+        {
+            if (person == null)
+            {
+                Console.WriteLine("Person is empty");
+                return;
+            }
+            if (head is null || tail is null)
+            {
+                Console.WriteLine("ListQueue is empty");
+                return;
+            }
+            if (index >= count)
+            {
+                Console.WriteLine("Out in the range");
+                return;
+            }
+
+            Node? cur = head;
+            for (int i = 0; i < count; i++)
+            {
+                if (index == i)
+                {
+                    cur!.Data = person;
+                    return;
+                }
+                cur = cur?.Next;
+            }
+            return;
+        }
+        public void GetMassive()
+        {
+            Sort();
+            Print();
+        }
+        public Person? Null(int index)
+        {
+            if (head is null || tail is null)
+            {
+                Console.WriteLine("ListQueue is empty");
+                return null;
+            }
+            if (index >= count)
+            {
+                Console.WriteLine("Out in the range");
+                return null;
+            }
+
+            Node? cur = head;
+            for (int i = 0; i < count; i++)
+            {
+                if (index == i)
+                {
+                    Person? person = cur?.Data;
+                    cur!.Data = null;
+                    return person;
+                }
+                cur = cur?.Next;
+            }
+            return null;
+        }
+        private void Sort()
+        {
+            for(int i = 1; i < count; i++)
+            {
+                Person? cur = Get(i);
+
+                if (cur is null) continue;
+
+                for (int j = i; j >= 0; j--)
+                {
+                    if (Get(j - 1) is null || Get(j) is null) continue;
+                    Set(Get(j - 1), j);
+                    if (j == 0)
+                    {
+                        Set(cur, j);
+                        break;
+                    }
+                    if (cur!.ToString()[0] > Get(j - 1)?.ToString()[0] || cur!.ToString()[0] == Get(j - 1)?.ToString()[0])
+                    {
+                        Set(cur, j);
+                        break;
+                    }
+                }
+            }   
+        }
+        public void Print()
+        {
+            Console.WriteLine();
+            Node? cur = head;
+            for (int i = 0; i < count; i++)
+            {
+                if (cur?.Data == null) Console.WriteLine("Пусто");
+                else Console.WriteLine(cur?.Data?.ToString());
+                cur = cur?.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
